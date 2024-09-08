2f327ff3db48b10540831ecb15cc3915b108ab41
DhruvChandar <167030713+DhruvChandar@users.noreply.github.com>
2024-09-09 05:15:34 +0530

    Finished shop page and routing to different categories

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes , Route } from "react-router-dom";
 import Home from "./routs/home/home.component";
 import Navigation from "./routs/navigation/navigation.component";
 import Authentication from "./routs/authentication/authentication.component";
-import Shop from "./components/shop/shop.component";
+import Shop from "./routs/shop/shop.component";
 import Checkout from "./routs/checkout/checkout.component";
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
