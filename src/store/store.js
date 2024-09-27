3007c336d810c802052674b031b123e7c1bda6f9
Mastermassive <gitmaster13@gmail.com>
2024-09-27 17:28:39 +0530

    added redux-persist and redux devtools access

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,31 @@
 import {compose, applyMiddleware} from "redux";
 import {legacy_createStore as createStore} from "redux";
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+    Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const presistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(presistConfig, rootReducer);
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
